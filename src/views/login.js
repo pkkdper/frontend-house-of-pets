07aae303fc160b47e888ab5a249b52cd683ca756
montibel <113550254+montibel@users.noreply.github.com>
2022-11-23 17:17:17 +0100

    login,submit

--- src/views/login.js
+++ src/views/login.js
@@ -1,14 +1,57 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import { useState } from "react";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
+import { useContext, useState } from "react";
+import { SessionContext } from "../contexts/SessionContext";
+import axios from "axios";
 
-function Login() {
+const LoginPage = () => {
+  /* const { setToken } = useContext(SessionContext); */
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await axios.post("http://localhost:5005/auth/login", {
+      username,
+      password,
+    });
+    console.log(response.data);
+    /* 
+    if (parsed.status === 200) {
+      setToken(parsed.token);
+    } else {
+      setError(parsed);
+    } */
+  };
+
   return (
-    <div className="App">
-      <Navbar />
-      <h1>Log in</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      {error?.message && <p>{error.message}</p>}
+      <TextInput
+        label="Username"
+        variant="filled"
+        size="md"
+        withAsterisk
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        required
+      />
+      <PasswordInput
+        label="Password"
+        variant="filled"
+        size="md"
+        withAsterisk
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        required
+      />
+      <Button type="submit" variant="light" color="cyan" size="md" uppercase>
+        Login
+      </Button>
+    </form>
   );
-}
+};
 
-export default Login;
+export default LoginPage;

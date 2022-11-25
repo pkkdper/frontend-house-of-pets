d32a328f4870ea187c376bb0027151e10b04c5cc
montibel <113550254+montibel@users.noreply.github.com>
2022-11-25 10:55:35 +0100

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HomeImage from "../assets/home.png";
-import { useContext } from "react";                     
-import { AuthContext } from "../contexts/auth.context"; 
+import { useContext } from "react";
+import { AuthContext } from "../contexts/auth.context";
 
 function Navbar() {
-  const { isLoggedIn, user } = useContext(AuthContext); 
+  const { isLoggedIn, user } = useContext(AuthContext);
   return (
     <nav>
-      <Link to="/">Home</Link><img src={HomeImage} alt="navbar-home" />
-      {isLoggedIn && (<>
-          <Link to="/auth/profile"> profile
-          </Link>        
-          <button>Logout</button>
-        </>
-      )}
+      <u>
+        <Link to="/">Home</Link>
+        <img src={HomeImage} alt="navbar-home" />
+        {isLoggedIn && (
+          <>
+            <Link to="/auth/profile"> Profile</Link>
+            <button>Logout</button>
+          </>
+        )}
         {!isLoggedIn && (
-        <>
-          <Link to="/auth/signup"> Sign Up </Link>
-          <Link to="/auth/login"> Login </Link>
-        </>
-      )}
-      
+          <>
+            <Link to="/auth/signup"> Sign Up </Link>
+            <Link to="/auth/login"> Login </Link>
+          </>
+        )}
+      </u>
     </nav>
   );
 }
--- src/views/login.js
+++ src/views/login.js
@@ -4,71 +4,80 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/auth.context";
 import axios from "axios";
 import Navbar from "../components/Navbar";
-const API_URL="http://localhost:5005/"
+const API_URL = "http://localhost:5005/";
+
 const LoginPage = (props) => {
-  const navigate = useNavigate()
   const { storeToken } = useContext(AuthContext);
-const LoginPage = () => {
-  const { setToken } = useContext(SessionContext);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState();
-  const navigate = useNavigate();
+  const LoginPage = () => {
+    const { setToken } = useContext(SessionContext);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState();
+    const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await axios.post("http://localhost:5005/auth/login", {
-      username,
-      password,
-    })
-    await((response)=> {
-      console.log(response.data.authToken)
-    });
-    console.log(response.data);
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const response = await axios.post("http://localhost:5005/auth/login", {
+        username,
+        password,
+      });
+      await ((response) => {
+        console.log(response.data.authToken);
+      });
+      console.log(response.data);
 
-    if (response) {
+      if (response) {
+        navigate("/auth/profile");
+      } else {
+        const errorDescription = error.response.data.message;
+        setError(errorDescription);
+      }
+
+      const parsed = response.data;
       navigate("/auth/profile");
-    } else {
-      const errorDescription = error.response.data.message;
-      setError(errorDescription);
-    }
+      if (parsed.status === 200) {
+        setToken(parsed.token);
+      } else {
+        setError(parsed);
+      }
+    };
 
-    const parsed = response.data;
-    navigate('/auth/profile');
-    // if (parsed.status === 200) {
-    //   setToken(parsed.token);
-    // } else {
-    //   setError(parsed);
-    // }
+    return (
+      <>
+        <Navbar />
+        <form onSubmit={handleSubmit}>
+          {error?.message && <p>{error.message}</p>}
+          <TextInput
+            label="Username"
+            variant="filled"
+            size="md"
+            withAsterisk
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            required
+          />
+          <PasswordInput
+            label="Password"
+            variant="filled"
+            size="md"
+            withAsterisk
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <Button
+            type="submit"
+            variant="light"
+            color="cyan"
+            size="md"
+            uppercase
+          >
+            Login
+          </Button>
+        </form>
+      </>
+    );
   };
-
-  return (<>
-  <Navbar/>
-    <form onSubmit={handleSubmit}>
-      {error?.message && <p>{error.message}</p>}
-      <TextInput
-        label="Username"
-        variant="filled"
-        size="md"
-        withAsterisk
-        value={username}
-        onChange={(event) => setUsername(event.target.value)}
-        required
-      />
-      <PasswordInput
-        label="Password"
-        variant="filled"
-        size="md"
-        withAsterisk
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        required
-      />
-      <Button type="submit" variant="light" color="cyan" size="md" uppercase>
-        Login
-      </Button>
-    </form>
-  </>);
-};}
+};
 
 export default LoginPage;

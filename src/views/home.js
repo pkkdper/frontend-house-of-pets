1df72bf9305258c3cb4e26333f9a8d772c52cd5e
Andreia <andreiacpafonso@gmail.com>
2022-11-27 20:55:54 +0000

    authToken, logoutready, addbuttons

--- src/views/home.js
+++ src/views/home.js
@@ -4,20 +4,8 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-//     <div>
-//       <Link to="/login">
-//         <h1>Log in</h1>
-//       </Link>
-//       <Link to="/signup">
-//         {" "}
-//         <h1>Sign up</h1>
-//       </Link>
-//       <Link to="/profile">
-//         <h1>Profile</h1>
-//       </Link>
-//     </div>
-<div><h2>Home</h2>
-<Navbar/></div>
+    <div><h2>Home</h2>
+      <Navbar /></div>
   );
 }
 
